504e359112de9ec6d537f1ea3600835abffd3266
fung-hei-man <94099670+fung-hei-man@users.noreply.github.com>
2023-04-18 00:29:58 +0800

    refactor: extract ATTENDANCE_STATUS

--- controller/employee.controller.js
+++ controller/employee.controller.js
@@ -2,6 +2,7 @@ const { ERROR_CODE } = require('../errorHandler/errorCode')
 const { ErrorException } = require('../errorHandler/errorException')
 const { checkDateFormat } = require('../helper/datetime.helper')
 const service = require('../service/employee.service')
+const {ATTENDANCE_STATUS} = require('../helper/attendance.dict')
 
 const queryByDateAndClockInTime = async (req, res, next) => {
   try {
@@ -26,7 +27,6 @@ const queryByDateAndClockInTime = async (req, res, next) => {
     res.send(result)
 
   } catch (err) {
-    console.error(err)
     next(err)
   }
 }
@@ -45,16 +45,15 @@ const queryByDateRangeAndClockInStatus = async (req, res, next) => {
     }
 
     let result = []
-    if (status === 'incomplete') {
+    if (status === ATTENDANCE_STATUS.IN_ONLY) {
       result = await service.getEmployeesByClockDateRangeAndNoClockOut(from, to)
     } else {
-      throw new ErrorException(ERROR_CODE.NOT_IMPLEMENTED, 'Only `incomplete` status has been implemented at the moment')
+      throw new ErrorException(ERROR_CODE.NOT_IMPLEMENTED, `Only \`${ATTENDANCE_STATUS.IN_ONLY}\` status has been implemented at the moment`)
     }
 
     res.send(result)
 
   } catch (err) {
-    console.error(err)
     next(err)
   }
 }
--- helper/attendance.dict.js
+++ helper/attendance.dict.js
@@ -1,8 +1,17 @@
+const ATTENDANCE_STATUS = {
+  IN_ONLY: 'in-only',
+  OUT_ONLY: 'out-only',
+  COMPLETE: 'complete',
+  UNKNOWN: 'unknown',
+  NO_UPDATE: 'no-update'
+}
+
 const CLOCK_TYPE = {
   IN: 'clock-in',
   OUT: 'clock-out'
 }
 
 module.exports = {
+  ATTENDANCE_STATUS,
   CLOCK_TYPE
 }
